aa2c260d8cc041025a567302dd6391382901b126
Ben Hibler <bhibler1@asu.edu>
2018-01-28 15:33:48 -0700

    Initial commit. (Youtube netbeans tutorial)

---
+++ src/Homework.java
@@ -0,0 +1,26 @@
+For each file (class) in your assignment, provide a heading (in comments) which
+includes:
+
+/*
+Assignment Number: Homework 1 – Programming with Java
+Benedetto Hibler
+
+
+• A description of what this program is doing.
+*/
+
+public class Homework 
+{
+
+  
+    public static void main(String[] args) 
+    {
+        public static int initializeArray()
+        {
+            
+        }
+    
+        
+    }
+    
+}
